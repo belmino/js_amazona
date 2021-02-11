63cfd68f266e65f1820813f4efbbf4fcf41806f2
belmino <belminoj@gmail.com>
2021-02-11 10:03:14 -0300

    Configuração do Mongodb e JWT

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,28 @@
 import express from 'express';
 import cors from 'cors';
-import data from './data';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
 
+import config from './config';
+import data from './data';
+import userRouter from './routers/userRouter';
+
+
+mongoose.connect(config.MONGODB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+}).then(() => {
+  console.log("Connected to mongodb.")
+})
+.catch((error) => {
+  console.log(error)
+})
 const app = express();
 
 app.use(cors());
-
+app.use(bodyParser.json());
+app.use('/api/users', userRouter)
 app.get('/api/products', (req, res) => {
   res.send(data.products);
 });
@@ -19,7 +36,10 @@ app.get('/api/products/:id', (req, res) => {
     res.status(404).send({message: 'Product Not Found!'})
   }
 });
-
+app.use((err, req, res, next) => {
+  const status = err.name && err.name === 'ValidationError' ? 400 : 500;
+  res.status(status).send({message: err.message})
+});
 app.listen(5000, () => {
   console.log('server at http://localhost:5000');
 });
